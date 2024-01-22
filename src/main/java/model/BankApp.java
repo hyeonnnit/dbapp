15ef90ca99095c60bfdcec4190b820af4a305d23
hyeon <lhyeonnit@gmail.com>
2024-01-22 17:08:27 +0900

    GET POST

--- src/main/java/model/BankApp.java
+++ src/main/java/model/BankApp.java
@@ -6,21 +6,44 @@ import db.DBConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Scanner;
 
 public class BankApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("삭제할 계좌번호를 입력해주세요: ");
-        int number = sc.nextInt();
-        BankDAO dao = new BankDAO();
-        int result = dao.deleteByNumber(number);
-        if (result ==1){
-            System.out.println("삭제 성공");
-        }else {
-            System.out.println("삭제 실패");
-        }
+        // http://bank.com/account GET
+        // http://bank.com/account POST
+        // http://bank.com/account/1 DELETE
+        // http://bank.com/account/1 PUT
+        System.out.print("메소드 입력: ");
+        String method = sc.nextLine();
+        System.out.print("식별자 입력: ");
+        String action = sc.nextLine();
+        String body = "";
+        BankDAO bankDAO = new BankDAO();
+        if (method.equals("GET")){
+            if (action.equals("/account")){
+                List<Account> accountList = bankDAO.selectAll();
+                System.out.println(accountList);
+            }else if (action.equals("/account/3")){
+                Account account = bankDAO.selectByNumber(3);
+                System.out.println(account);
+            }
+        } else if (method.equals("POST")) {
+            System.out.print("body data insert: ");
+            body = sc.nextLine();
+            String[] st1 = body.split("&");
+            String password = st1[0].split("=")[1];
+            int balance = Integer.parseInt(st1[1].split("=")[1]);
+            if (action.equals("/account")){
+                bankDAO.insert(password,balance);
+            }
+        } else if (method.equals("PUT")) {
 
+        } else if (method.equals("DELETE")) {
+
+        }
     }
 }
 
